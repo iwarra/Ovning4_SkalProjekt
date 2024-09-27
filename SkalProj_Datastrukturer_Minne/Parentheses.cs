2c939ef2a9bdd56463afadaa6ce81b96e4909d26
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-09-27 14:42:29 +0200

    Refactored the parentheses class

--- SkalProj_Datastrukturer_Minne/Parentheses.cs
+++ SkalProj_Datastrukturer_Minne/Parentheses.cs
@@ -1,10 +1,27 @@
+using System;
+using System.Collections.Generic;
+
 namespace SkalProj_Datastrukturer_Minne
 {
-    public class Parentheses()
+
+
+    public class Parentheses
     {
-        public char[] Round = ['(', ')'];
-        public char[] Curly = ['{', '}'];
-        public char[] Square = ['[', ']'];
+        public string Name { get; set; }
+        public char Opening {  get; set; }
+        public char Closing { get; set; }
+
+        public Parentheses(string name, char opening, char closing)
+        {
+            Name = name;
+            Opening = opening;
+            Closing = closing;
+        }
+
+        public override string ToString()
+        {
+            return $"Name='{Name}', Opening='{Opening}', Closing='{Closing}'";
+        }
     }
 }
 
